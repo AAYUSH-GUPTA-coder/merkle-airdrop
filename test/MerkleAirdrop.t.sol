630189726410fef66ffacb0e36ec15997d0842ef
Aayush Gupta <guptaaayush432@gmail.com>
2025-01-02 18:59:37 +0530

    added logic for zksync chain

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -17,8 +17,9 @@ contract MerkleAirdropTest is ZkSyncChainChecker, Test {
     bytes32 proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
     bytes32 proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
     bytes32[] public PROOF = [proofOne, proofTwo];
-    address user;
-    uint256 userPrivKey;
+    address public gasPayer;
+    address public user;
+    uint256 public userPrivKey;
 
     function setUp() public {
         if (!isZkSyncChain()) {
@@ -31,6 +32,7 @@ contract MerkleAirdropTest is ZkSyncChainChecker, Test {
             token.transfer(address(airdrop), AMOUNT_TO_MINT);
         }
         (user, userPrivKey) = makeAddrAndKey("user");
+        gasPayer = makeAddr("gasPayer");
     }
 
     function testUserCanClaim() public {
@@ -38,8 +40,15 @@ contract MerkleAirdropTest is ZkSyncChainChecker, Test {
         uint256 startingBalance = token.balanceOf(user);
         console.log("starting balance", startingBalance);
 
-        vm.prank(user);
-        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
+        //create message digest
+        bytes32 digest = airdrop.getMessageHash(user, AMOUNT_TO_CLAIM);
+
+        // user sign the message and give the claim power to gasPayer Address
+        // No need vm.prank(user) beacuse we are already using the private key of the user
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivKey, digest);
+
+        vm.prank(gasPayer);
+        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF, v, r, s);
 
         uint256 endingBalance = token.balanceOf(user);
         console.log("ending balance", endingBalance);
